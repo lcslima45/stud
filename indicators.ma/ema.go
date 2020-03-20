2e9ceed7d9c7c0f6dbe0dd0308eae3b1ba745cf4
lcslima45 <lcslima45@gmail.com>
2020-03-20 13:10:07 -0300

    primeiros passos no OnNextBar do EMA

--- indicators.ma/ema.go
+++ indicators.ma/ema.go
@@ -8,10 +8,15 @@ import (
 // TEMA - Media movel Exponencial
 type TEMA struct {
 	valores     [][]float64     	// Buffer par armazenar os valroes calculados
-	bars        []entities.Bar  	// Armazena as barras  
+	bars        []entities.Bar  	// Armazena as barras
 	valLastTick float64         	// Variavel auxiliar par otimizacao de recalculo da media  - so calcular se for diferente do valor anterior
 	MaPeriod    int             	// Periodo da media movel exponencial 
 	SourcePrice entities.PriceType 	// Preço referencia para calulo da média movel exponencial
+	Open        float64
+	Close		float64
+	High		float64
+	Low			float64
+
 }
 
 //GetLenth - total de posicoes do buffer
@@ -101,11 +106,71 @@ func (i *TEMA) Init(parameters ...interface{}) error {
 //OnNextBar - deve ser efocado a cada negociação do ativo target
 //Parametro bar - barra atual
 func (i *TEMA) OnNextBar(bar entities.Bar) error {
-
+	//No começo a nova barra tem os preços semelhantes ao fechamento da barra anterior.
+	i.Open  = bar.Close
+	i.Low   = bar.Close
+	i.High  = bar.Close
+	i.Close = bar.Close
+	i.valLastTick = bar.Close
+	newBar := entities.Bar{}
+	//verificando se o vetor de valores de i é vazio
+	if len(i.valores) == 0 {
+		i.valores = make([][]float64, 1)
+	}
 	// Adicionar um elemento no inicio do buffer
-	i.valores[0] = append([]float64{0}, i.valores[0]...)
+	if len(i.valores) > 0 {
+		i.valores[0] = append([]float64{0}, i.valores[0]...)
+	} else {
+		i.valores[0] = append(i.valores[0], 0)
+	}
 
-	// Retorna erro nulo
+	i.bars[0] = bar
+	i.bars[0] = append([]entities.Bar{newBar}, i.bars...)
+	//se houverem barras suficientes realizar o cálculo da média exponencial
+	var mediaExp float64
+	if len(i.bars) > i.MaPeriod {
+		var novoPreco float64
+		k := 2/float64(i.MaPeriod + 1)
+		prevMediaEx := i.GetValue(0, 1)
+
+		switch i.SourcePrice {
+
+		case entities.Open:
+			novoPreco = bar.Open
+
+		case entities.Low:
+			novoPreco = bar.Low
+
+		case entities.Close:
+			novoPreco = bar.Close
+		}
+	} else if (len(i.bars)-1) == i.MaPeriod {
+		// calcular Média Simples como base para a Média exponencial base
+		var summa float64
+		switch i.SourcePrice {
+		case entities.Open:
+			for j := 1; j<=i.MaPeriod; j++ {
+				summa = summa + i.bars[j].Open
+			}
+		case entities.Low:
+			for j := 1; j<=i.MaPeriod; j++ {
+				summa = summa + i.bars[j].Low
+			}
+		case entities.Close:
+			for j := 1; j<= i.MaPeriod; j++ {
+				summa = summa + i.bars[j].Close
+			}
+		case entities.High:
+			for j := 1; j <= i.MaPeriod; j++ {
+				summa = summa + i.bars[j].High
+			}
+		}
+		mediaSimples := summa/float64(i.MaPeriod)
+		i.SetValue(0, 0, mediaSimples)
+	} else {
+		i.SetValue(0,0,0)
+	}
+	//Retorna erro nulo
 	return nil
 
 }
